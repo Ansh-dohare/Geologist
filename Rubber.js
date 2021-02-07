9873af5981b7153eb6c9ab88459f277e51998767
Ansh-dohare <76939009+Ansh-dohare@users.noreply.github.com>
2021-02-06 21:27:42 -0800

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,29 @@
+class Rubber{
+	constructor(x,y,z)
+	{
+		var options={
+			restitution:0.3,
+			friction:5,
+			density:1
+		}
+		this.x=x;
+		this.y=y;
+		this.z=z
+		this.body=Bodies.circle(this.x, this.y, (this.z-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			ellipse(0,0,this.z, this.z);
+			pop()
+	}
+
+}
